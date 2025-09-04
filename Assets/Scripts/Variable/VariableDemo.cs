a7f042e98461757a110b08db429869fd22c06bc6
DESKTOP-5V3FRMH\M <nanoyuni0222@gmail.com>
2025-09-04 15:20:56 +0900

    변수 정의

---
+++ Assets/Scripts/Variable/VariableDemo.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class VariableDemo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //[1][2] 변수 선언과 동시에 값 대입(초기화)
+        int number = 7;
+        Debug.Log(number);
+    }
+}
